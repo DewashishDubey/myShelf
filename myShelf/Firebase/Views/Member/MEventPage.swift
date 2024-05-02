bf1a8c56781a8d6bb17b8f9ddd6adab80f046cfe
Shivesh Rathore <rathoreshivesh22@gmail.com>
2024-05-02 12:16:00 +0530

    MEventsPage

--- myShelf/Firebase/Views/Member/MEventPage.swift
+++ myShelf/Firebase/Views/Member/MEventPage.swift
@@ -9,174 +9,146 @@ import SwiftUI
 
 struct MEventPage: View {
     var body: some View {
-        ZStack {
-            Color.black.edgesIgnoringSafeArea(.all)
-            //VStack {
-                //ImagePlaceholder()
-                   // .frame(height: 300)
-                ScrollView {
-                    ImagePlaceholder()
-                        .frame(height: 300)
-                        //.ignoresSafeArea(.all)
-                    EventDetails()
-                }.ignoresSafeArea()
-           // }
-        }
+        VStack(){
+            ScrollView(showsIndicators: false){
+                ImagePlaceholder()
+                EventDetails()
+            }
+
+        }.ignoresSafeArea()
     }
 }
 
 struct ImagePlaceholder: View {
     var body: some View {
-        ZStack {
-            Image("library")
-                .resizable()
-                .aspectRatio(contentMode: .fill)
-                .frame(maxWidth: .infinity)
-                .clipped()
-                .overlay(
-                    LinearGradient(
-                        gradient: Gradient(colors: [Color.black.opacity(0.2), Color.black.opacity(0.9)]),
-                        startPoint: .top,
-                        endPoint: .bottom
-                    )
-                    
+        Image("library")
+            .resizable()
+            .aspectRatio(contentMode: .fill)
+            .clipped()
+            .overlay(
+                LinearGradient(
+                    gradient: Gradient(colors: [Color.black.opacity(0.2), Color.black.opacity(0.9)]),
+                    startPoint: .top,
+                    endPoint: .bottom
                 )
-        }
-        }
+            )
+    }
 }
+
+
 
 
 
 struct EventDetails: View {
     var body: some View {
-        VStack(alignment: .leading, spacing: 16) {
+        
             Text("Book Reading & Signing Event")
-              .font(
-                Font.custom("SF Pro", size: 22)
-                  .weight(.semibold)
-              )
-              .multilineTextAlignment(.center)
-              .foregroundColor(.white)
-              .padding(.leading,64)
-           
-            HStack {
+                .font(.custom("SF Pro", size: 22))
+                .padding(.bottom,30)
+        
+        VStack(alignment: .leading, spacing: 30){
+            HStack{
+                HStack(alignment: .center, spacing: 10) {
+                    Image(systemName: "calendar")
+                }
+                .padding(.horizontal, 10)
+                .padding(.vertical, 12)
+                .background(Color(red: 0.26, green: 0.52, blue: 0.96))
+                .cornerRadius(6)
                 
-                Image(systemName: "calendar")
-                    .font(.system(size: 30))
-                    .foregroundColor(.blue)
-                
-                    .padding(.leading,25)
-                    .padding(.top,40)
-                VStack{
+                VStack(alignment: .leading, spacing: 10){
                     Text("19 January 2024")
-                    .font(
-                    Font.custom("SF Pro Text", size: 15)
-                    .weight(.medium)
+                        .font(.custom("SF Pro Text", size: 14)
+                          .weight(.medium)
                     )
-                    .foregroundColor(.white)
-                        .padding(.top,40)
-                    
                     Text("Tuesday, 04:00 PM")
-                      .font(
-                        Font.custom("SF Pro Text", size: 15)
+                      .font(.custom("SF Pro Text", size: 12)
                           .weight(.medium)
                       )
                       .foregroundColor(.white.opacity(0.6))
-                            .padding(.leading,20)
-                    
-                    
                 }
-            }
-                
+            }.padding(.horizontal)
             
-            
-            HStack {
-                Image(systemName: "location")
-                    .foregroundColor(.blue)
-                    .font(.system(size: 30))
-                    .padding(.top,40)
-                    .padding(.leading,25)
+            HStack{
+                HStack(alignment: .center, spacing: 10) {
+                    Image(systemName: "location")
+                }
+                .padding(.horizontal, 10)
+                .padding(.vertical, 12)
+                .background(Color(red: 0.26, green: 0.52, blue: 0.96))
+                .cornerRadius(6)
                 
-                VStack{
+                VStack(alignment: .leading, spacing: 10){
                     Text("The National Library of India")
-                      .font(
-                        Font.custom("SF Pro Text", size: 15)
+                        .font(.custom("SF Pro Text", size: 14)
                           .weight(.medium)
-                      )
-                      .foregroundColor(.white)
-                        .padding(.top,40)
-                        .padding(.leading,20)
-                    
-                    
+                    )
                     Text("Kolkata, West Bengal")
-                      .font(
-                        Font.custom("SF Pro Text", size: 15)
-                          .weight(.medium)
+                      .font(.custom("SF Pro Text", size: 12)
+
                       )
                       .foregroundColor(.white.opacity(0.6))
-                        .padding(.trailing,25)
                 }
-            }
+            }.padding(.horizontal)
             
             
-            
-            HStack {
-                Image(systemName: "person")
-                    .foregroundColor(.blue)
-                    .font(.system(size: 30))
-                    .padding(.top,40)
-                    .padding(.leading,25)
-                VStack{
-                    
+            HStack{
+                HStack(alignment: .center, spacing: 10) {
+                    Image(systemName: "person")
+                }
+                .padding(.horizontal, 11)
+                .padding(.vertical, 12)
+                .background(Color(red: 0.26, green: 0.52, blue: 0.96))
+                .cornerRadius(6)
+                
+                VStack(alignment: .leading, spacing: 10){
                     Text("J. K. Rowling")
-                      .font(
-                        Font.custom("SF Pro Text", size: 15)
+                        .font(.custom("SF Pro Text", size: 14)
                           .weight(.medium)
-                      )
-                      .foregroundColor(.white)
-                        .padding(.top,40)
-                        .padding(.leading,25)
+                    )
                     Text("Author")
-                      .font(
-                        Font.custom("SF Pro Text", size: 15)
-                          .weight(.medium)
+                      .font(.custom("SF Pro Text", size: 12)
+
                       )
                       .foregroundColor(.white.opacity(0.6))
-                      .padding(.trailing,20)
-                        
                 }
-            }
+            }.padding(.horizontal)
             
             
-            Button(action: {}) {
-                Text("Register")
-                    .frame(maxWidth: .infinity)
-                    .padding()
+                Button(action: {
+                }) {
+                    HStack(alignment: .center, spacing: 10) {
+                        Image(systemName: "plus.circle.fill")
+                            .foregroundColor(.white)
+                        Text("Register")
+                            .foregroundColor(.white)
+                    }
+                    .padding(10)
+                    .frame(maxWidth: .infinity, alignment: .center)
                     .background(Color(red: 0.26, green: 0.52, blue: 0.96))
-                    .foregroundColor(.white)
-                    .cornerRadius(20)
-                    .padding(.top,40)
-            }
+                    .cornerRadius(6)
+            }.padding(.horizontal)
             
             Text("Event Overview")
-              .font(
-                Font.custom("SF Pro Text", size: 15)
-                  .weight(.semibold)
-              )
-              .foregroundColor(.white)
-              .padding(.leading,30)
+                .font(.custom("SF Pro Text", size: 16)
+                .weight(.semibold)
+                )
+                .frame(maxWidth: .infinity, alignment: .topLeading)
+                .padding(.horizontal)
             
             Text("Prepare to be transported into the enchanting world of literature as the National Library of India proudly presents \"Magical Literary Evening: A Conversation with J.K. Rowling.\" Renowned author J.K. Rowling, celebrated for her iconic Harry Potter series, will grace the stage for an unforgettable evening of inspiration and imagination.\n\nThe event will feature Rowling herself, offering insights into her creative process, the magical universe of Harry Potter, and her latest literary endeavors. Attendees will have the opportunity to delve into the depths of Rowling's literary genius, exploring the themes, characters, and inspirations that have captured the hearts of millions worldwide.\n\nThe evening will include a captivating discussion moderated by esteemed literary scholars, providing a deeper understanding of Rowling's literary contributions and the enduring impact of her work on readers of all ages. Following the conversation, Rowling will engage with the audience in a special Q&A session, offering fans the chance to interact directly with the beloved author.\n\nAdditionally, attendees will have the exclusive opportunity to purchase signed copies of Rowling's books and merchandise, ensuring a cherished memento of this magical literary experience. Don't miss this rare chance to embark on a journey through the realms of fantasy and imagination with one of the world's most beloved authors at the National Library of India's \"Magical Literary Evening: A Conversation with J.K. Rowling.\"")
-              .font(Font.custom("SF Pro Text", size: 15))
-              .foregroundColor(.white.opacity(0.6))
-              .padding(.leading,30)
+              .font(Font.custom("SF Pro Text", size: 14))
+              .frame(maxWidth: .infinity, alignment: .topLeading)
+              .padding(.horizontal)
+            
         }
-        .padding()
     }
 }
+
 struct MemberEventPage_Previews: PreviewProvider {
     static var previews: some View {
         MEventPage()
+            .preferredColorScheme(.dark)
     }
 }
 
