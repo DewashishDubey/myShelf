bfbc0e56af52ac2e026dedc9609de8bd86fa82f2
DewashishDubey <dewasishdubey@gmail.com>
2024-05-07 12:27:42 +0530

    minor updates

--- myShelf/TestView.swift
+++ myShelf/TestView.swift
@@ -5,41 +5,132 @@
 //  Created by Dewashish Dubey on 03/05/24.
 //
 
+
 import SwiftUI
 
-struct EventListView: View {
-    @StateObject var viewModel = EventViewModel()
+struct booksmatrix: View {
+    @State private var count: Int = 500
+    private let maxCount: Int = 3546
     
     var body: some View {
-        ZStack{
-            Color.black.ignoresSafeArea(.all)
-                List(viewModel.events) { event in
-                    VStack(alignment: .leading) {
-                        Text("Title: \(event.title)")
-                            .font(.headline)
-                        Text("Summary: \(event.summary)")
-                            .foregroundColor(.gray)
-                        Text("Address: \(event.address)")
-                        Text("Capacity: \(event.capacity)")
-                        Text("Guest: \(event.guest)")
-                        Text("Room: \(event.room)")
-                        Text("Selected Date: \(event.selectedDate)")
-                        Text("Selected Time: \(event.selectedTime)")
-                        Text("UID: \(event.uid)")
-                    }
+        ZStack {
+            Color.black.edgesIgnoringSafeArea(.all)
+            VStack {
+                Spacer()
+                if count > maxCount {
+                    SemicircleProgressView3(progress: CGFloat(count)/CGFloat(maxCount))
+                        .frame(width: 300, height: 150)
+                    
+                }
+                else{
+                    SemicircleProgressView(progress: CGFloat(count) / CGFloat(maxCount))
+                        .frame(width: 300, height: 150)
+                }
+                Text("Complete your Collection")
+                .font(
+                Font.custom("SF Pro Text", size: 14)
+                .weight(.medium)
+                )
+                .foregroundColor(.white)
+                Text("\(count)/\(maxCount)")
+                    .foregroundColor(.white)
+                    .font(.largeTitle)
                     .padding()
+                Button(action: {
+                    // Action to complete the collection
+                }) {
+                    Text("Complete your Collection")
+                    .font(
+                    Font.custom("SF Pro Text", size: 10)
+                    .weight(.medium)
+                    )
+                    .foregroundColor(.white)
+                    .padding()
+                    .background(Color.blue)
+                    .cornerRadius(25)
+
+                    
                 }
-                .navigationTitle("Events")
-                .onAppear {
-                    viewModel.fetchEvents()
-                }
-            
+                
+                Spacer()
+            }
         }
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        EventListView()
+struct SemicircleProgressView: View {
+    var progress: CGFloat
+    
+    var body: some View {
+        GeometryReader { geometry in
+            Path { path in
+                let width = geometry.size.width
+                let height = geometry.size.height
+                path.addArc(center: CGPoint(x: width / 2, y: height),
+                            radius: width / 2,
+                            startAngle: Angle(degrees: 180),
+                            endAngle: Angle(degrees: 360),
+                            clockwise: false)
+            }
+            .stroke(style: StrokeStyle(lineWidth: 20, lineCap: .round))
+            .fill(Color.gray.opacity(0.2))
+            
+        
+            
+            Path { path in
+                let width = geometry.size.width
+                let height = geometry.size.height
+                path.addArc(center: CGPoint(x: width / 2, y: height),
+                            radius: width / 2,
+                            startAngle: Angle(degrees: 0),
+                            endAngle: Angle(degrees: 360 - (Double(progress) * 180.0)),
+                            clockwise: true)
+            }
+            .stroke(style: StrokeStyle(lineWidth: 20, lineCap: .round))
+            .fill(Color.blue)
+        }
     }
 }
+
+struct SemicircleProgressView3: View {
+    var progress: CGFloat
+    
+    var body: some View {
+        GeometryReader
+        { geometry in
+            Path { path in
+                let width = geometry.size.width
+                let height = geometry.size.height
+                path.addArc(center: CGPoint(x: width / 2, y: height),
+                            radius: width / 2,
+                            startAngle: Angle(degrees: 180),
+                            endAngle: Angle(degrees: 360),
+                            clockwise: false)
+            }
+            .stroke(style: StrokeStyle(lineWidth: 20, lineCap: .round))
+            .fill(Color.gray.opacity(0.2))
+            
+        
+            
+            Path { path in
+                let width = geometry.size.width
+                let height = geometry.size.height
+                path.addArc(center: CGPoint(x: width / 2, y: height),
+                            radius: width / 2,
+                            startAngle: Angle(degrees: 0),
+                            endAngle: Angle(degrees: 180),
+                            clockwise: true)
+            }
+            .stroke(style: StrokeStyle(lineWidth: 20, lineCap: .round))
+            .fill(Color.blue)
+        }
+    }
+}
+
+
+#Preview {
+    booksmatrix()
+        .preferredColorScheme(.dark)
+}
+
+
